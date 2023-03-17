883f9506334a482e6359c1a2ad2a16b860a2320d
Dan Walkes <danwalkes@trellis-logic.com>
2023-03-17 11:56:41 -0600

    Work in progress event webhook
    Not yet complete/tested

--- examples/EventWebhooks/src/EventWebhooks.py
+++ examples/EventWebhooks/src/EventWebhooks.py
@@ -1,40 +1,92 @@
 import traceback
-import time
+import re
+import requests
 
 class EventWebhooks:
+    class HookAction():
+        def __init__(self, url = 'localhost', json = None, headers = 'Content-Type: application/json'):
+            self.url = url
+            self.json = json
+
+        def invoke(self):
+            requests.post(self.url, json = self.json)
+
+    class RegexWebhook():
+        def __init__(self, name = None, sensor_name_match = None, sensor_content_match = None,
+                        hook = None):
+            self.name = name
+            self.sensor_name_match = sensor_name_match
+            self.sensor_content_match = None
+            self.hook = hook
+
+        def run_match_action(self, message):
+            match = False
+            if self.sensor_name_match and 'sensorEvents' in message:
+                sensor_events = message['sensorEvents']
+                if self.sensor_name_match:
+                    for key in self.sensor_name_match.keys():
+                        if key in sensor_events and \
+                                self.sensor_name_match[key] in sensor_events[key]:
+                            if self.sensor_content_match:
+                                events = sensor_events[key][self.sensor_name_match[key]]
+                                for event in events:
+                                    for key in self.sensor_content_match:
+                                        if key in event:
+                                            if len(re.findall(self.sensor_content_match[key],
+                                                      str(event[key]))):
+                                                match = True
+                            else:
+                                match = True
+            if match and self.hook:
+                self.hook.invoke()
+
     def __init__(self) -> None:
-        pass
+        self.webhooks = []
+        self.webhooks.append(EventWebhooks.RegexWebhook(
+            name = "SignalOn",
+            sensor_name_match =
+            {
+                "presenceSensor" : "D4146053-308A-47F5-809A-4244AA86F6B9"
+            }
+            sensor_content_match =
+            {
+                "updateCount" : 1
+            }
+            hook =
+            EventWebhooks.HookAction(
+                url="ntcip-relay-server",
+                json={
+                    "phase_control_group" : 1,
+                    "phase" : 1,
+                    "signal_type" : True,
+                    "mib" : "1.3.6.1.4.1.1206.4.2.1.1.5.1.7"
+                })
+        ))
+        self.webhooks.append(EventWebhooks.RegexWebhook(
+            name = "SignalOff",
+            sensor_name_match =
+            {
+                "presenceSensor" : "D4146053-308A-47F5-809A-4244AA86F6B9"
+            }
+            sensor_content_match =
+            {
+                "endedAt" : ".*"
+            }
+            hook = EventWebhooks.HookAction(
+                url="ntcip-relay-server",
+                json={
+                    "phase_control_group" : 1,
+                    "phase" : 1,
+                    "signal_type" : False,
+                    "mib" : "1.3.6.1.4.1.1206.4.2.1.1.5.1.7"
+                })
+        ))
+
+
     def callback(self, message):
-        try:
-            sourceId = message.get("sourceId", "unknown")
-            frameId = message.get("frameId", "unknown")
-            analyticsTimestamp = message.get("analyticsTimestamp",0)
-            timestamp_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(analyticsTimestamp/1000))
-            output = f"Message from source: {sourceId} frame: {frameId} at {timestamp_str}\n"
-
-            metaclasses = message.get("metaClasses", {})
-            if metaclasses:
-                output += " With objects:\n"
-            for metaclass, objects in metaclasses.items():
-                for object_id, object in objects.items():
-                    object_class = object.get("class", "unknown")
-                    box = object.get("box", {})
-                    x = box.get("x", -1)
-                    y = box.get("y", -1)
-                    output += f"  - {metaclass}:{object_class} ({object_id})  at ({x},{y})\n"
-
-            mediaEvents = message.get("mediaEvents", {})
-            if mediaEvents:
-                output += " With media events:\n"
-            for event in mediaEvents:
-                    type = event.get("type", "unknown")
-                    sequence = event.get("sequence", -1)
-                    output += f"  - {type} ({sequence})\n"
-
-            output += "---------------\n"
-            print(output)
-
-
-        except Exception as e:
-            print(f"Caught exception {e} handling callback")
-            traceback.print_exc()
+        for wh in self.webhooks:
+            try:
+                wh.run_match_action(message)
+            except Exception as e:
+                print(f"Caught exception {e} handling {wh.name} webhook")
+                traceback.print_exc()
