921dff9da73a9b3126c95ba6b342d7330bd44bae
Dan Walkes <danwalkes@trellis-logic.com>
2024-08-12 13:47:06 -0600

    Go to release 1.1 of event-webhooks
    With fix for restart on connectivity loss.
    Also rework container to remove pip install requirement, which
    is currently failing, and modify to use slim python container instead.

--- examples/EventWebhooks/build.sh
+++ examples/EventWebhooks/build.sh
@@ -3,7 +3,7 @@ pushd $(dirname $0)
 repo=ghcr.io
 namespace=trellis-logic
 image_name=event-webhooks
-tag=1.0
+tag=1.1
 buildargs="--build-arg basedir=EventWebhooks/"
 if [[ "$@" != *"--push"* ]]; then
     buildargs="$buildargs --output=type=docker"
